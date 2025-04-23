17967012d8eee6bd9400470ea7b85447af2af7db
Goutam kumar <goutam54401@gmail.com>
2025-04-23 19:12:39 +0530

    first commit

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int n = sc.nextInt();
+        for(int i = n ; i>=1 ; i--){
+            
+        }
+        
+    }
+}
