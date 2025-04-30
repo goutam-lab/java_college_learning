808360cdc8d292787a24447e5bc1ac6d95c243a2
Goutam kumar <goutam54401@gmail.com>
2025-05-01 00:17:54 +0530

    Array completed

--- array.java
+++ array.java
@@ -1,8 +1,41 @@
-public class array {
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
-    }
-}
+// public class array {
+//     public static void main(String[] args) {
+//         int[] arr = {1, 2, 3, 4, 5};
+//         for (int i = 0; i < arr.length; i++) {
+//             System.out.println(arr[i]);
+//         }
+//     }
+// }
+
+
+// single dimensional array pratice
+// public class array {
+//     public static void main(String[] args){
+//         int[] arr = {1, 2, 3, 4, 5};
+//         int n = 0;
+//         for (int i = 0 ; i < arr.length ; i++){
+//             n = arr[i] + n;
+//         }
+//         System.out.println("Sum of the array is: " + n);
+//     }
+// }
+
+
+//Write a program to print the transpose of a given 3x3 matrix.
+// public class array {
+//     public static void main(String[] args){
+//         int[][] matrix = {
+//             {1, 2, 3},
+//             {4, 5, 6},
+//             {7, 8, 9}
+//         };
+//         for(int i = 0 ; i < matrix.length; i++){
+//             for(int j = 0 ; j < matrix[i].length ; j++){
+//                 System.out.print(matrix[j][i] + " ");
+//             }
+//             System.out.println();
+//         }
+
+//     }
+
+// }
